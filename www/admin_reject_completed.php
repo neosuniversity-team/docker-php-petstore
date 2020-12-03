1176fbd5e68cbd9b17070a58e7ef9398cd96153f
hhugohm <hhugohm@gmail.com>
2020-12-02 19:00:39 -0600

    clean branch

--- www/admin_reject_completed.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include('includes/dbconn.php');
-   $id = $_GET['id'];
-   $sql = ("DELETE FROM tblorders where id = '$id'") or die (mysqli_error());
-   $result=mysqli_query($con, $sql);
-   header("location:index.php");
-?>
